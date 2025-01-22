35b7d6a2e83ac2a9cd12bc13ee2f6b6c9f177139
racersmith@gmail.com <racersmith@gmail.com>
2025-01-22 23:36:07 +0000

    Created _README and env_var.jpg; Edited environ and settings; Deleted Form1

--- server_code/environ.py
+++ server_code/environ.py
@@ -1,21 +1,20 @@
 from anvil.tables import app_tables
 
-def set(variable: str, value, info: str | None = None):
+
+def set(variable: str, value, info: str | None = None) -> None:
     # Set an environment variable
-    return (
+    (
         app_tables.env.get(key=variable)
         or app_tables.env.add_row(key=variable, value=value, info=info)
     )
 
+
 def get(variable: str, default=LookupError):
-    """ Get an environment variable """
+    """Get an environment variable"""
     row = app_tables.get(key=variable)
     if row is None:
         if default is LookupError:
             raise LookupError(f"'{variable}' not found")
         else:
             return default
-    return row['value']
-
-
-        
+    return row["value"]
