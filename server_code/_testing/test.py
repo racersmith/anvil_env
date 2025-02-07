4f404d6074c1706f30598f595e6894d08ae977f1
racersmith@gmail.com <racersmith@gmail.com>
2025-02-07 18:32:27 +0000

    Edited _testing.test

--- server_code/_testing/test.py
+++ server_code/_testing/test.py
@@ -1,13 +1,23 @@
+from anvil import app
 
-def run():
-    """ Run all tests in the tests module 
+"""
+Expose an endpoint to run tests at when we are in a debug environment.
+You will need to publish the debug version before this can be accessed.
+"""
 
-    example:
-        from ._testing import test
-        test.run()
-    """
-    from . import tests
-    import anvil_testing
+if "debug" in app.environment.tags and app.id == 'AFKPYPDLJMH2TYVK':
+    import anvil.server
+    test_endpoint = "/test"
+    print("Tests can be run here:")
+    print(f"{anvil.server.get_app_origin('debug')}{test_endpoint}")
 
-    anvil_testing.auto.run(tests, quiet=False)
+    function_name = f"route:{test_endpoint}"
+    from anvil._server import registrations
     
+    @anvil.server.route(test_endpoint)
+    def run() -> anvil.server.HttpResponse:
+        from . import tests
+        import anvil_testing
+
+        results = anvil_testing.auto.run(tests, quiet=False)
+        return anvil.server.HttpResponse(body=results)
