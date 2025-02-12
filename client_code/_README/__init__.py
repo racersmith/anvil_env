764213c65d319213036ca9a9957d43923a7ea5d4
racersmith@gmail.com <racersmith@gmail.com>
2025-02-12 22:32:36 +0000

    Created LICENSE.txt; Edited _README

--- client_code/_README/__init__.py
+++ client_code/_README/__init__.py
@@ -1,8 +1,6 @@
-import anvil.server
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
 from ._anvil_designer import _READMETemplate
+
+
 class _README(_READMETemplate):
     def __init__(self, **properties):
         self.init_components(**properties)
